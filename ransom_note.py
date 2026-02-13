5c8aa0ef40e5b893c731b1438e09ada3e8ebe18b
Manuel Alfaro <manuel.alfaro247@gmail.com>
2026-02-13 12:06:25 -0600

    Complete ransom note hash table lab

--- ransom_note.py
+++ ransom_note.py
@@ -10,4 +10,15 @@ def can_construct(ransomNote: str, magazine: str) -> bool:
     Returns:
         bool: True if ransomNote can be constructed, False otherwise.
     """
-    pass  # TODO: Implement this function
+
+    letter_counts = {}
+
+    for char in magazine:
+        letter_counts[char] = letter_counts.get(char, 0) + 1
+
+    for char in ransomNote:
+        if letter_counts.get(char, 0) == 0:
+            return False
+        letter_counts[char] -= 1
+
+    return True
